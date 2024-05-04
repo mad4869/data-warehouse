704be7e6897fc442e72c722c187f0f80ff929597
MAkbarD <mad4869@gmail.com>
2024-05-04 23:57:23 +0700

    Added script file to run ELT pipeline

---
+++ run_elt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "========== Start dbt with Luigi Orchestration Process =========="
+
+# Virtual Environment Path
+VENV_PATH="/home/mad4869/Documents/pacmann/data-storage/data-warehouse/dwh-venv/bin/activate"
+
+# Activate Virtual Environment
+source "$VENV_PATH"
+
+# Set Python script
+PYTHON_SCRIPT="/home/mad4869/Documents/pacmann/data-storage/data-warehouse/elt_pipeline.py"
+
+# Get Current Date
+current_datetime=$(date '+%d-%m-%Y_%H-%M')
+
+# Append Current Date in the Log File
+log_file="/home/mad4869/Documents/pacmann/data-storage/data-warehouse/logs/elt/elt_$current_datetime.log"
+
+# Run Python Script and Insert Log
+python "$PYTHON_SCRIPT" >> "$log_file" 2>&1
+
+echo "========== End of dbt with Luigi Orchestration Process =========="
