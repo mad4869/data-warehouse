b791af1e02ea7e0f06e157e46d2e30aea4f413be
MAkbarD <mad4869@gmail.com>
2024-05-06 08:02:59 +0700

    Reintegrated Sentry

--- elt.py
+++ elt.py
@@ -1,18 +1,17 @@
 import luigi
+import sentry_sdk
 
 from pipeline.extract import Extract
 from pipeline.load import Load
 from pipeline.transform import DbtDebug, DbtDeps, DbtRun, DbtSnapshot, DbtTest
 
+sentry_sdk.init(
+    dsn="https://245f925fd0fe886dbe02afd9033caaa6@o414765.ingest.us.sentry.io/4507199769804800",
+    enable_tracing=True,
+)
+
 if __name__ == "__main__":
-    luigi.build([
-        Extract(),
-        Load(),
-        DbtDebug(),
-        DbtDeps(),
-        DbtRun(),
-        DbtSnapshot(),
-        DbtTest()
-        ],
-        local_scheduler=True
+    luigi.build(
+        [Extract(), Load(), DbtDebug(), DbtDeps(), DbtRun(), DbtSnapshot(), DbtTest()],
+        local_scheduler=True,
     )
